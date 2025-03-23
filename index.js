a7cba1385dfadd4e21c7fba2281bbb4b7b350ecd
harsh-developer <harshpratap1444@gmail.com>
2025-03-23 21:21:17 +0530

    final changes for deployment

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const route = require('./route/route');
+const route = require('./src/route/route');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
 
 dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 // CORS Configuration
 app.options('*', cors()); // Handle preflight requests
 app.use(cors({
-    origin: '*',  // Allow requests from Angular
+    origin: '*',  // Allow requests from all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
-    credentials: true  // Allow credentials like cookies, authorization headers
+    credentials: true  // allow credentials like cookies, authorization headers
 }));
 
 app.use(express.json());
@@ -24,6 +28,14 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use("/", route);
 
+app.get('/test', (req, res) => {
+    res.send('Hello World!');
+
+})
+
 app.listen(process.env.PORT || 5000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 5000));
 });
+
+
+// module.exports.handler = serverless(app);
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const dotenv = require('dotenv');
 const { isValid, isValidMail, isValidMobile, isValidRequest, isValidPassword } = require("../validator/validation");
 
